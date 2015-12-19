ee20b09724e3970811bb19a82a200f014df7f357
agupta2017 <agupta2017@pingry.org>
2015-12-18 21:41:56 -0500

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -2,6 +2,8 @@ public class Runner
 {
   public static void main(String[] args)
   {
-    GameBoard g = new GameBoard();
+    //In the code the blank space is represented as a 9
+    int[][] i = {{1,2,3},{4,5,6},{7,8,9}}
+    GameBoard g = new GameBoard(i);
   }
 }
